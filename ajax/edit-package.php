9a8d81cd7b902a95ac45fe7ae6b4cd18d5ded3d2
Carson Calcutt <46988440+carsoncalcutt@users.noreply.github.com>
2024-05-16 09:21:23 -0400

    Fix day training expiration bug

--- ajax/edit-package.php
+++ ajax/edit-package.php
@@ -14,14 +14,19 @@ if (isset($_POST['id']) AND isset($_POST['owner']) AND isset($_POST['currentStat
   $expirationException=$row_expiration_exception['expirationException'];
   $sql_edit_package="UPDATE owners_packages SET status='$status' WHERE ownerPackageID='$packageID'";
   $conn->query($sql_edit_package);
+  $sql_package_info="SELECT totalDays, durationDays, durationMonths FROM packages p JOIN owners_packages op USING (packageID) WHERE ownerPackageID='$packageID'";
+  $result_package_info=$conn->query($sql_package_info);
+  $row_package_info=$result_package_info->fetch_assoc();
   if ($currentStatus==='Not Started' AND $status==='Active') {
-    $sql_package_info="SELECT totalDays, durationDays, durationMonths FROM packages p JOIN owners_packages op USING (packageID) WHERE ownerPackageID='$packageID'";
-    $result_package_info=$conn->query($sql_package_info);
-    $row_package_info=$result_package_info->fetch_assoc();
     if ($row_package_info['totalDays']>0) {
       $daysLeft=$row_package_info['totalDays'];
-      $durationDays=$row_package_info['durationDays'];
-      $durationMonths=$row_package_info['durationMonths'];
+      if ($row_package_info['durationDays']>0 AND $row_package_info['durationMonths']>0) {
+        $durationDays=$row_package_info['durationDays'];
+        $durationMonths=$row_package_info['durationMonths'];
+      } else {
+        $durationDays=NULL;
+        $durationMonths=NULL;
+      }
     } else {
       $daysLeft=NULL;
       $durationDays=NULL;
@@ -29,7 +34,7 @@ if (isset($_POST['id']) AND isset($_POST['owner']) AND isset($_POST['currentStat
     }
     if (isset($startDate) AND $startDate!='' AND $durationDays>0 AND $durationMonths>0) {
       $startDate=$_POST['startDate'];
-      if ($expirationException=='Yes') {
+      if ($expirationException=='Yes' OR $durationDays===0 OR $durationDays===NULL OR $durationMonths===0 OR $durationMonths===NULL) {
         $expirationDate=NULL;
       } else {
         if ($currentStatus==='Not Started' AND $status==='Active') {
@@ -60,6 +65,9 @@ if (isset($_POST['id']) AND isset($_POST['owner']) AND isset($_POST['currentStat
   if (isset($_POST['startDate']) AND $_POST['startDate']!='') {
     $sql_edit_package_start="UPDATE owners_packages SET startDate='$startDate' WHERE ownerPackageID='$packageID'";
     $conn->query($sql_edit_package_start);
+  }
+  if ($row_package_info['durationDays']==0 AND $row_package_info['durationMonths']==0) {
+    $expirationDate=NULL;
   }
   $sql_edit_package_expiration="UPDATE owners_packages SET expirationDate='$expirationDate' WHERE ownerPackageID='$packageID'";
   $conn->query($sql_edit_package_expiration);
